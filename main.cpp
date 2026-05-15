4fc49fe38670bff3212d993fe3f297e35c253bd0
cr1lex <semen1lebedev@gmail.com>
2026-05-15 14:02:28 +0300

    added function fib(n): recursively calculates the n-th number (the sequence starts with 1, 1, ...)

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n) {
+    if (n <= 2) return 1;
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "fib(10) = " << fib(10) << endl;
+    return 0;
+}
